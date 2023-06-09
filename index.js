59bbe66289f51a7bf57521dc8e67bd4e78bee0bb
Meredith Jonatan <mjonatan@me.com>
2023-06-09 05:11:51 -0400

    isolated each line as its own array

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const { parse } = require("path");
 const readline = require("readline");
+
 const csvFilePath = "./input/case-study-data.csv"
-const newArray = "./functions.js"
+// const newArray = "./functions.js"
 
 function parseFunction () {
     console.log("hiya!")
@@ -10,12 +11,10 @@ function parseFunction () {
     const stream = fs.createReadStream(csvFilePath);
     const reader = readline.createInterface({ input: stream });
 
-
-    let data = [];
-
     reader.on("line", row => {
         // This will split a row string into an array
         // And then push into the data array
+        let data = [];
         data.push(row.split(","));
         console.log(data)
       });
