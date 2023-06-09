9558cf51034d404ada9f9ff228b65a08b0fe9f92
Meredith Jonatan <mjonatan@me.com>
2023-06-09 13:52:58 -0400

    json object now holding values correctly

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 const fs = require("fs");
-const { parse } = require("path");
 const readline = require("readline");
-
 const csvFilePath = "./input/case-study-data.csv"
 // const newArray = "./functions.js"
 
 function parseFunction () {
-    console.log("hiya!")
+    console.log("function initialized 🚀")
 
     const stream = fs.createReadStream(csvFilePath);
     const reader = readline.createInterface({ input: stream });
 
     reader.on("line", row => {
-        // This will split a row string into an array
-        // And then push into the data array
+        // split document by rows
+
+        // create array (must be nested here for scope)
         let data = [];
+        
+        // push row content into data array        
         data.push(row.split(","));
-        console.log(data)
+
+        // data array must be converted into an object
+        // error handling happens as array is being converted
+
+        let dataObj = {
+            name: "",
+            color: "",
+            size: "",
+            price: ""            
+            };
+
+        //convert array into json string
+        const jsonObj = JSON.stringify(Object.assign({}, data[0]));
+
+        // console.log(data)
+        console.log(jsonObj)
       });
 
       reader.on("close", () => {
         //  Reached the end of file
-        // console.log(data);
+        console.log("function complete 🥳");
       });
 
 };
