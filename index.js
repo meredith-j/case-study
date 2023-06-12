2d4b17e0e16ad5d8e3df66bfa7a8477e68ec4362
Meredith Jonatan <mjonatan@me.com>
2023-06-12 18:47:59 -0400

    update error conditions to account for more specific error logs)

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const fs = require("fs");
 const readline = require("readline");
 const { v4: uuidv4 } = require('uuid');
 const csvFile = "./input/case-study-data.csv"
-const nodemailer = require('nodemailer')
+const {countDecimals} = "./functions.js"
+const nodemailer = require('nodemailer');
+const { count } = require("console");
 
 function parseFunction (csvFilePath) {
 
@@ -36,18 +38,24 @@ function parseFunction (csvFilePath) {
 
         // error handling occurs
 
+        // create regex variable to check decimal points for price column
+        let decimalChecker = /^[0-9]+\.[0-9]{2}$|[0-9]+\.[0-9]{2}[^0-9]/;
+
         // check if row is header
         if (data[0][1] === "Device Number") {
             return
         }
 
-        // check if model name, color or size columns are empty or are not a string
-        else if (data[0][0] == "" || data[0][2] == "" || data[0][3] == "" || !isNaN(data[0][0]) || !isNaN(data[0][2]) || !isNaN(data[0][3])) {
+        // check if model name column is empty or not a string
+        else if (data[0][0] == "" || !isNaN(data[0][0])) {
 
             // log error
             errors ++
 
             console.log("error detected", data[0])
+
+            // append spreadsheet row for CSV errors output
+            data[0][0] = "";
 
             fs.appendFile("./output-errors/csv-errors.csv", `\n${data[0].toString()}`, "utf8", function(err){
                 if (err) {
@@ -59,12 +67,15 @@ function parseFunction (csvFilePath) {
         }
 
         // check if model number column is empty or not a number
-        else if (data[0][1] == "" || Number.isInteger(data[0][1]) || data[0][1] % 1 !== 0) {
+        else if (data[0][1] == "" || data[0][1].includes('.') || Number.isInteger(data[0][1]) || data[0][1] % 1 !== 0) {
 
             // log error
             errors ++
 
             console.log("error detected", data[0])
+
+            // append spreadsheet row for CSV errors output
+            data[0][1] = "";
 
             fs.appendFile("./output-errors/csv-errors.csv", `\n${data[0].toString()}`, "utf8", function(err){
                 if (err) {
@@ -75,12 +86,56 @@ function parseFunction (csvFilePath) {
             return
         }
 
-        // check if price column is empty or not data type: float
-        else if (data[0][4] === "" || isNaN(data[0][4]) || parseFloat(data[0][4]) % 1 == false) {
+        // check if color column is empty or not a string
+        else if (data[0][2] == "" || !isNaN(data[0][2])) {
+
+            // log error
+            errors ++
+
+            console.log("error detected", data[0])
+
+            // append spreadsheet row for CSV errors output
+            data[0][2] = "";
+
+            fs.appendFile("./output-errors/csv-errors.csv", `\n${data[0].toString()}`, "utf8", function(err){
+                if (err) {
+                    console.log(err)
+                }
+            })
+
+            return
+        }
+
+        // check if size column is empty or not a string
+        else if (data[0][3] == "" || !isNaN(data[0][3])) {
+
+            // log error
+            errors ++
+
+            console.log("error detected", data[0])
+
+            // append spreadsheet row for CSV errors output
+            data[0][3] = "";
+
+            fs.appendFile("./output-errors/csv-errors.csv", `\n${data[0].toString()}`, "utf8", function(err){
+                if (err) {
+                    console.log(err)
+                }
+            })
+
+            return
+        }
+
+        // check if price column is empty or a float
+        else if (data[0][4] === "" || !data[0][4].includes('.') || isNaN(data[0][4])
+        || decimalChecker.test(data[0][4]) == false) {
 
             // log error 
             errors ++
             console.log("error detected", data[0])
+
+            // append spreadsheet row for CSV errors output
+            data[0][4] = "";
 
             fs.appendFile("./output-errors/csv-errors.csv", `\n${data[0].toString()}`, "utf8", function(err){
                 if (err) {
@@ -93,29 +148,28 @@ function parseFunction (csvFilePath) {
         // if no errors, row is converted to json file
          else {
 
-        // data array is converted into an object
-        let dataObj = {
-            deviceId: `${data[0][0]}-${data[0][1]}`,
-            color: data[0][2],
-            size: data[0][3],
-            price: Number(data[0][4])            
-            };
+            // data array is converted into an object
+            let dataObj = {
+                deviceId: `${data[0][0]}-${data[0][1]}`,
+                color: data[0][2],
+                size: data[0][3],
+                price: +data[0][4]
+                };
 
-        //create unique name for each row of csv/json file
-        let jsonFile = uuidv4() + ".json"
+            //create unique name for each row of csv/json file
+            let jsonFile = uuidv4() + ".json"
 
-        // convert dataObj to json and save new file
-        fs.writeFileSync('./output-json/' + jsonFile, JSON.stringify(Object.assign({}, dataObj)), (err) => {
-            if (err) {
-                console.log(err)
-            }
+            // convert dataObj to json and save new file
+            fs.writeFileSync('./output-json/' + jsonFile, JSON.stringify(Object.assign({}, dataObj)), (err) => {
+                if (err) {
+                    console.log(err)
+                }
 
-            else return
-         });
+                else return
+            });
 
-         console.log("row success 🤓", dataObj)
+            console.log("row success 🤓", dataObj)
         }   
-
 
       });
 
